8c7d2be7dc113ebe0c9df1db198f415725ae59b7
Landuber Kassa <Landuber.Kassa@patlive.com>
2014-03-24 19:04:44 -0700

    Intermediate results

--- src/app/screens/board.js
+++ src/app/screens/board.js
@@ -6,10 +6,12 @@ define(['lodash', 'q', 'common/viewUtil', 'facades/boardFacade'],
         // Vertext Shader Program
         var VSHADER_SOURCE =
             'attribute vec4 a_Position;\n' +
-            'attribute float a_PointSize;\n' +
+            'uniform float u_CosB, u_SinB;' +
             'void main() {\n' +
-            '   gl_Position = a_Position;\n' + //Coordinates
-        '   gl_PointSize = a_PointSize;\n' +
+            '   gl_Position.x = a_Position.x * u_CosB - a_Position.y * u_SinB;\n' +
+            '   gl_Position.y = a_Position.x * u_SinB + a_Position.y * u_CosB;\n' +
+            '   gl_Position.z = a_Position.z;\n' +
+            '   gl_Position.w = 1.0;\n' +
             '}\n';
 
         // Fragment shader program
@@ -39,42 +41,36 @@ define(['lodash', 'q', 'common/viewUtil', 'facades/boardFacade'],
                     return;
                 }
 
-                // Get the storage location of attribute variable
-                var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-
-                 if (a_Position < 0) {
-                    console.log('Failed to get the storage location of a_Position');
-                    return;
-                }
-                var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-
-                if (a_PointSize < 0) {
-                    console.log('Failed to get the storage location of a_PointSize');
-                    return;
-                }
-
                 var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-                if(!u_FragColor) {
+                if (!u_FragColor) {
                     console.log('Failed to get u_FragColor variable');
                     return;
                 }
-               
 
+                gl.clearColor(0.0, 0.0, 0.0, 1.0);
+                gl.clear(gl.COLOR_BUFFER_BIT);
 
-                gl.vertexAttrib1f(a_PointSize, 10.0);
+                initVertexBuffers(gl, u_FragColor);
 
 
                 canvas.onmousedown = function(ev) {
-                    click(ev, gl, canvas, a_Position, u_FragColor);
+                    click(ev, gl, canvas, u_FragColor);
                 };
-                gl.clearColor(0.0, 1.0, 1.0, 1.0);
-                gl.clear(gl.COLOR_BUFFER_BIT);
+
 
                 viewUtil.showScreen('home-screen');
             });
         }
 
-        function click(ev, gl, canvas, a_Position, u_FragColor) {
+        function click(ev, gl, canvas, u_FragColor) {
+
+            // Get the storage location of attribute variable
+            var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+            if (a_Position < 0) {
+                console.log('Failed to get the storage location of a_Position');
+                return;
+            }
 
             var x = ev.clientX;
             var y = ev.clientY;
@@ -86,9 +82,9 @@ define(['lodash', 'q', 'common/viewUtil', 'facades/boardFacade'],
 
             g_points.push([x, y]);
 
-            if(x >= 0.0 && y >= 0.0) {
+            if (x >= 0.0 && y >= 0.0) {
                 g_colors.push([1.0, 0.0, 0.0, 1.0]);
-            } else if(x < 0.0 && y < 0.0) {
+            } else if (x < 0.0 && y < 0.0) {
                 g_colors.push([0.0, 1.0, 0.0, 1.0]);
             } else {
                 g_colors.push([1.0, 1.0, 1.0, 1.0]);
@@ -109,6 +105,41 @@ define(['lodash', 'q', 'common/viewUtil', 'facades/boardFacade'],
                 // Draw a point 
                 gl.drawArrays(gl.POINTS, 0, 1);
             }
+        }
+
+        function initVertexBuffers(gl, u_FragColor) {
+
+            gl.uniform4f(u_FragColor, 1.0, 1.0, 0.0, 1.0);
+            var vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5]);
+
+            var vertexBuffer = gl.createBuffer();
+            if (!vertexBuffer) {
+                console.log("Failed to create a vertex buffer.");
+                return -1;
+            }
+
+            //bind the buffer
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+            var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+            gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(a_Position);
+
+            ininRotationParams(gl);
+            gl.drawArrays(gl.TRIANGLES, 0, 3);
+        }
+
+        function ininRotationParams(gl) {
+            var ANGLE = 90; // degrees
+            var radian = Math.PI * ANGLE / 180;
+
+            var u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+            var u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+
+            gl.uniform1f(u_SinB, Math.sin(radian));
+            gl.uniform1f(u_CosB, Math.cos(radian));
         }
 
         return {
